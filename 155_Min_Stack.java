25cf76001bee71281c3de2e0d1aad827b034cbaf
Shuvadip Dian <shuvadipdian112@gmail.com>
2024-07-30 01:18:00 +0530

    Min stack

---
+++ 155_Min_Stack.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+class MinStack {
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack;
+    public MinStack() {
+        stack= new Stack();
+    }
+    
+    public void push(int val) {
+       if(val <= min){          
+            stack.push(min);
+            min=val;
+        }
+        stack.push(val);
+    }
+    
+    public void pop() {
+        if(stack.pop() == min) min=stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
