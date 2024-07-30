b1d852131bf2466b5df17decec86d541b4164262
Shuvadip Dian <shuvadipdian112@gmail.com>
2024-07-31 01:13:37 +0530

    Basic Calculator

---
+++ 224_Basic_Calculator.java
@@ -0,0 +1,60 @@
+class Solution {
+    int idx; 
+    public int calculate(String s) {
+        idx = 0; 
+        return calc(s);
+    }
+    
+    private int calc(String s) {
+        int res = 0, num = 0, sign = 1;
+        while (idx < s.length()) {
+            char c = s.charAt(idx++);
+            if (c >= '0' && c <= '9') num = num * 10 + c - '0';
+            else if (c == '(') num = calc(s); 
+            else if (c == ')') return res + sign * num;
+            else if (c == '+' || c == '-') { 
+                res += sign * num;
+                num = 0;
+                sign = c == '-' ? -1 : 1;
+            }
+        }
+        return res + sign * num;
+    }
+}
+
+// class Solution {
+//     public int calculate(String s) {
+//         Stack<Integer> stack = new Stack<Integer>();
+//         int result = 0;
+//         int number = 0;
+//         int sign = 1;
+//         for(int i = 0; i < s.length(); i++){
+//             char c = s.charAt(i);
+//             if(Character.isDigit(c)){
+//                 number = 10 * number + (int)(c - '0');
+//             }else if(c == '+'){
+//                 result += sign * number;
+//                 number = 0;
+//                 sign = 1;
+//             }else if(c == '-'){
+//                 result += sign * number;
+//                 number = 0;
+//                 sign = -1;
+//             }else if(c == '('){
+//                 //we push the result first, then sign;
+//                 stack.push(result);
+//                 stack.push(sign);
+//                 //reset the sign and result for the value in the parenthesis
+//                 sign = 1;   
+//                 result = 0;
+//             }else if(c == ')'){
+//                 result += sign * number;  
+//                 number = 0;
+//                 result *= stack.pop();   
+//                 result += stack.pop();   
+//             }
+//         }
+//         if(number != 0) result += sign * number;
+//         return result;
+//     }
+// }
