9fba92b04d933d97c78ac57b6e3d8573c16914e1
Shuvadip Dian <shuvadipdian112@gmail.com>
2024-08-09 03:03:10 +0530

    Find minimum in rotated sorted array

---
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findMin(int[] nums) {
+      int left = 0;
+      int right = nums.length - 1;
+      int mid=0;
+      while (left < right) {
+        mid = (left + right) / 2;
+        if (nums[mid] < nums[right])
+          right = mid;
+        else
+          left = mid + 1;
+      }
+      return nums[left];
+    }
+  }
